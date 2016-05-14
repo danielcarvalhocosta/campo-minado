f643ebaa7389bac906c0ad8d10ca6e61e1d94cfa
Myron David Lucena Campos Peixoto <myrondavid@hotmail.com>
2016-05-14 18:04:18 -0300

    Adição de tela, nada definitivo, só alguns testes
    Signed-off-by: Myron David Lucena Campos Peixoto <myrondavid@hotmail.com>

--- src/main.c
+++ src/main.c
@@ -6,17 +6,38 @@
 #define BOMBAS_FACIL 20
 #define BOMBAS_DIFICIL 50
 
+void jogar(){
+	int opc;
+	system("cls"); //limpa a tela
+	printf("Escolha o nivel de dificuldade\n");
+	printf("1 - Facil :)\n");
+	printf("2 - Dificil :(\n");
+	printf("3 - Voltar ao menu principal\n");
+	printf("Opcao selecionada: ");
+	scanf("%d", &opc);
+	//if(opc == 3) break;
+}
+
 int main() {
 	casa campo[DIMENSAO_CAMPO][DIMENSAO_CAMPO];
+	int opc;
+	do{
+		system("cls"); //limpa a tela
+  	printf("Campo Minado\n\n");
+  	printf("1 - Jogar\n");
+  	printf("2 - Ajuda\n");
+  	printf("3 - Sair\n");
+  	printf("\nO que deseja fazer? ");
+  	scanf("%d", &opc);
 
-  printf("Campo Minado\n\n");
+		switch (opc) {
+			case 1:
+				jogar();
+			break;
+			case 2:
+			break;
+		}
 
-  printf("1 - Jogar\n");
-  printf("2 - Ajuda\n");
-  printf("3 - Sair\n");
-
-  printf("\nO que deseja fazer? ");
-  getchar();
-
+	}	while(opc != 3);
 	return 0;
 }
